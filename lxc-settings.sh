8dfb5635a7bb2d92313391baef0558d235345293
Tariq Mehmood <tariqmalik323@gmail.com>
2024-12-29 18:42:29 +0500

    Update lxc-settings.sh

--- lxc-settings.sh
+++ lxc-settings.sh
@@ -6,4 +6,4 @@ adduser admin
 
 usermod -aG sudo admin
 
-apt install -y ca-certificates curl gnupg lsb-release ntp htop zip unzip net-tools ncdu apache2-utils
+apt install -y ca-certificates curl gnupg lsb-release ntp htop zip unzip net-tools ncdu apache2-utils iputils-ping
